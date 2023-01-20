0971ab73f0ae485d0d703d240e9eefca3e9c5d7b
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-01-20 13:38:02 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,17 @@
+function highAndLow(numbers){
+   const arr = numbers.split(" ")
+    let maxNum = arr[0]
+    let minNum = arr[0]
+
+    for(let num of arr) {
+        if(Number(num) > maxNum) {
+            maxNum = num
+        }
+
+        if(Number(num) < minNum) {
+            minNum = num
+        }
+    }
+
+    return maxNum + " " + minNum
+}
